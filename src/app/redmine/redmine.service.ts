621da13bbfebf2c26b11c0d51e933c34f8f04586
Manuel Tobío <teymanu@gmail.com>
2022-10-12 00:59:29 +0200

    c

--- src/app/redmine/redmine.service.ts
+++ src/app/redmine/redmine.service.ts
@@ -18,8 +18,8 @@ export class RedmineService {
     }
   ): Observable<RedmineIssue[]> {
     let url = redmineUrl.endsWith('/')
-      ? 'http://cors.io/?' + redmineUrl
-      : 'http://cors.io/?' + redmineUrl + '/';
+      ? 'https://cors.io/?' + redmineUrl
+      : 'https://cors.io/?' + redmineUrl + '/';
     // let url = '';
     url = url + 'issues.json';
 
