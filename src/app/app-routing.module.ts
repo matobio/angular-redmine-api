eaa1a112bf7bc330bb939e6b69a8fa6479698593
Manuel Tobío <teymanu@gmail.com>
2022-10-11 22:43:06 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RedmineService } from './redmine/redmine.service';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
